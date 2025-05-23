24cf3ee01f3d93438af9796018a9a2874d39fab3
El-Wafi-Cherkaoui <elwaficherkaoui@gmail.com>
2025-05-23 15:24:34 +0100

    Initial commit : Setup laravel api

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::get('/e', function (Request $request) {
+    return 'xsaxa';
+});
