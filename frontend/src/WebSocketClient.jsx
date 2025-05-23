07c1a72b93d91cb4e0ed289b4f1748cefe72ec7c
El-Wafi-Cherkaoui <elwaficherkaoui@gmail.com>
2025-05-23 16:50:05 +0100

    Setup Websocket connection between Laravel Api and frontend server

---
+++ frontend/src/WebSocketClient.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+// import { add_to_saved_data } from '../redux/slices/GlobalDataSlice';
+
+
+export default function WebSocketClient() {
+  useEffect(() => {
+
+    window.Pusher = Pusher;
+    window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    // wssPort: 8080,
+    // enabledTransports: ['ws', 'wss'],
+
+    forceTLS: false,
+    disableStats: false,
+    });
+
+    window.Echo.channel('added-items-channel')
+      .listen('.stock',async (e) => {
+      })
+
+    return () => {
+      window.Echo.leaveChannel('added-items-channel');
+      window.Echo.leaveChannel('removed-items-channel');
+      window.Echo.leaveChannel('updated-items-channel');
+    };
+  }, []);
+
+
+  return (
+   ''
+  );
+}
